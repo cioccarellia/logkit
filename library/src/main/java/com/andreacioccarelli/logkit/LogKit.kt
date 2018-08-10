7d2f0c83255ad78bb3d22cecfcdb4f04a1f0bc55
Andrea Cioccarelli <cioccarelliandrea01@gmail.com>
2018-08-10 13:21:06 +0200

    Added code parameters in functions

--- library/src/main/java/com/andreacioccarelli/logkit/LogKit.kt
+++ library/src/main/java/com/andreacioccarelli/logkit/LogKit.kt
@@ -16,6 +16,9 @@ fun logd(vararg items: Any?) {
     }
 }
 
+@SuppressLint("LogConditional")
+fun logd(code: () -> Any?) = Log.d("$LOGGING_TAG $DEBUG_TAG", code().toString())
+
 /**
  * Error log call.
  *
@@ -29,6 +32,9 @@ fun loge(vararg items: Any?) {
     }
 }
 
+@SuppressLint("LogConditional")
+fun loge(code: () -> Any?) = Log.e("$LOGGING_TAG $ERROR_TAG", code().toString())
+
 /**
  * Warning log call.
  *
@@ -41,6 +47,9 @@ fun logw(vararg items: Any?) {
         Log.w("$LOGGING_TAG $WARNING_TAG", item.toString())
     }
 }
+
+@SuppressLint("LogConditional")
+fun logw(code: () -> Any?) = Log.w("$LOGGING_TAG $WARNING_TAG", code().toString())
 
 /**
  * Info log call.
@@ -56,6 +65,9 @@ fun logi(vararg items: Any?) {
     }
 }
 
+@SuppressLint("LogConditional")
+fun logi(code: () -> Any?) = Log.i("$LOGGING_TAG $INFO_TAG", code().toString())
+
 /**
  * Assert log call.
  *
@@ -69,3 +81,6 @@ fun loga(vararg items: Any?) {
         Log.wtf("$LOGGING_TAG $ASSERT_TAG", item.toString())
     }
 }
+
+@SuppressLint("LogConditional")
+fun loga(code: () -> Any?) = Log.wtf("$LOGGING_TAG $ASSERT_TAG", code().toString())
