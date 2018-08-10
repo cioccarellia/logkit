519d0e439e59f966ab2499614404d3196458e2c0
Andrea Cioccarelli <cioccarelliandrea01@gmail.com>
2018-08-10 14:27:56 +0200

    Added nullability annotations

--- library/src/main/java/com/andreacioccarelli/logkit/LogKit.kt
+++ library/src/main/java/com/andreacioccarelli/logkit/LogKit.kt
@@ -1,7 +1,9 @@
 package com.andreacioccarelli.logkit
 
 import android.annotation.SuppressLint
+import android.support.annotation.NonNull
 import android.util.Log
+import org.jetbrains.annotations.Nullable
 
 /**
  * Debug log call.
@@ -10,14 +12,14 @@ import android.util.Log
  * The color for this call is Green [#66BB6A]
  * */
 @SuppressLint("LogConditional")
-fun logd(vararg items: Any?) {
+fun logd(@Nullable vararg items: Any?) {
     for (item in items) {
         Log.d("$LOGGING_TAG $DEBUG_TAG", item.toString())
     }
 }
 
 @SuppressLint("LogConditional")
-fun logd(code: () -> Any?) = Log.d("$LOGGING_TAG $DEBUG_TAG", code().toString())
+fun logd(@NonNull code: () -> Any?) = Log.d("$LOGGING_TAG $DEBUG_TAG", code().toString())
 
 /**
  * Error log call.
@@ -26,14 +28,14 @@ fun logd(code: () -> Any?) = Log.d("$LOGGING_TAG $DEBUG_TAG", code().toString())
  * The color for this call is Red [#EF5350]
  * */
 @SuppressLint("LogConditional")
-fun loge(vararg items: Any?) {
+fun loge(@Nullable vararg items: Any?) {
     for (item in items) {
         Log.e("$LOGGING_TAG $ERROR_TAG", item.toString())
     }
 }
 
 @SuppressLint("LogConditional")
-fun loge(code: () -> Any?) = Log.e("$LOGGING_TAG $ERROR_TAG", code().toString())
+fun loge(@NonNull code: () -> Any?) = Log.e("$LOGGING_TAG $ERROR_TAG", code().toString())
 
 /**
  * Warning log call.
@@ -42,14 +44,14 @@ fun loge(code: () -> Any?) = Log.e("$LOGGING_TAG $ERROR_TAG", code().toString())
  * The color for this call is Yellow [#FFA726]
  * */
 @SuppressLint("LogConditional")
-fun logw(vararg items: Any?) {
+fun logw(@Nullable vararg items: Any?) {
     for (item in items) {
         Log.w("$LOGGING_TAG $WARNING_TAG", item.toString())
     }
 }
 
 @SuppressLint("LogConditional")
-fun logw(code: () -> Any?) = Log.w("$LOGGING_TAG $WARNING_TAG", code().toString())
+fun logw(@NonNull code: () -> Any?) = Log.w("$LOGGING_TAG $WARNING_TAG", code().toString())
 
 /**
  * Info log call.
@@ -59,14 +61,14 @@ fun logw(code: () -> Any?) = Log.w("$LOGGING_TAG $WARNING_TAG", code().toString(
  * The color for this call is Blue [#00B0FF]
  * */
 @SuppressLint("LogConditional")
-fun logi(vararg items: Any?) {
+fun logi(@Nullable vararg items: Any?) {
     for (item in items) {
         Log.i("$LOGGING_TAG $INFO_TAG", item.toString())
     }
 }
 
 @SuppressLint("LogConditional")
-fun logi(code: () -> Any?) = Log.i("$LOGGING_TAG $INFO_TAG", code().toString())
+fun logi(@NonNull code: () -> Any?) = Log.i("$LOGGING_TAG $INFO_TAG", code().toString())
 
 /**
  * Assert log call.
@@ -75,12 +77,11 @@ fun logi(code: () -> Any?) = Log.i("$LOGGING_TAG $INFO_TAG", code().toString())
  * The color for this call is Purple [#BA68C8]
  * */
 @SuppressLint("LogConditional")
-fun loga(vararg items: Any?) {
-    assert(true)
+fun loga(@Nullable vararg items: Any?) {
     for (item in items) {
         Log.wtf("$LOGGING_TAG $ASSERT_TAG", item.toString())
     }
 }
 
 @SuppressLint("LogConditional")
-fun loga(code: () -> Any?) = Log.wtf("$LOGGING_TAG $ASSERT_TAG", code().toString())
+fun loga(@NonNull code: () -> Any?) = Log.wtf("$LOGGING_TAG $ASSERT_TAG", code().toString())
